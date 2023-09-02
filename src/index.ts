87b968e0ecc287f14b890830d84990cf1edf9f3d
Diego <gimbernat13@gmail.com>
2023-09-01 21:10:03 -0500

    removed adding to database

--- src/index.ts
+++ src/index.ts
@@ -6,21 +6,11 @@ import { Request, Response } from "express"
 
 
 AppDataSource.initialize()
-  .then(async () => {
-    const category1 = new Category()
-    category1.name = "TypeScript"
-    await AppDataSource.manager.save(category1)
-    const category2 = new Category()
-    category2.name = "Programming"
-    await AppDataSource.manager.save(category2)
-    const post = new Post()
-    post.title = "TypeScript"
-    post.text = `TypeScript is Awesome!`
-    post.categories = [category1, category2]
-    await AppDataSource.manager.save(post)
-    console.log("Post has been saved: ", post)
+  .then(() => {
+    console.log("✅ Initialized data source")
+    // here you can start to work with your database
   })
-  .catch((error) => console.log("Error: ", error))
+  .catch((error) => console.log(error))
 
 // create and setup express app
 const app = express();
