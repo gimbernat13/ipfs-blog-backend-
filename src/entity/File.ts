10953a7d34fe00c6b24ca6f9d3cf9603099f2718
Diego <gimbernat13@gmail.com>
2023-09-18 23:44:41 -0500

    🏗️ File Entity

---
+++ src/entity/File.ts
@@ -0,0 +1,15 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from "typeorm";
+import { User } from "./User";  // Import the User entity
+
+@Entity()
+export class File {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  cid: string;
+
+  // Many-to-one relationship with User
+  @ManyToOne(() => User, user => user.files)
+  user: User;
+}
