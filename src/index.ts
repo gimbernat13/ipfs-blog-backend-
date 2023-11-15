75eb51282afecd33e872825e0059bfcf3495f791
Diego <gimbernat13@gmail.com>
2023-11-15 11:31:10 -0500

    added cors

--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,60 @@
-
-import { AppDataSource } from "./data-source";
-import * as dotenv from "dotenv";
-import * as express from "express";
-import userRoutes from './routes/user.routes';
-import fileRoutes from './routes/file.routes';
-import postRoutes from './routes/post.routes';
-import nonceRoutes from './routes/nonce.routes';
-import { ethers } from "ethers";
-import { verifyMessage } from "./utils/verifyWeb3Message";
-
-
-
+import { AppDataSource } from "./data-source"
+import * as dotenv from "dotenv"
+import * as express from "express"
+import userRoutes from "./routes/user.routes"
+import fileRoutes from "./routes/file.routes"
+import postRoutes from "./routes/post.routes"
+import nonceRoutes from "./routes/nonce.routes"
+import { ethers } from "ethers"
+import { verifyMessage } from "./utils/verifyWeb3Message"
+const cors = require("cors")
 
 // Generate a random wallet for testing (Do not use in production!)
-const wallet = ethers.Wallet.createRandom();
+const wallet = ethers.Wallet.createRandom()
 
 // FIXME: Change Mock values
-const mockMessage = 'Hello, this is a test message!';
-const mockAddress = wallet.address; // Use the generated address
-const mockPrivateKey = wallet.privateKey;
+const mockMessage = "Hello, this is a test message!"
+const mockAddress = wallet.address // Use the generated address
+const mockPrivateKey = wallet.privateKey
 
 var mockSignature: any = null
 
-wallet.signMessage(mockMessage)
-  .then(signature => {
-    console.log("Signature:", signature);
+wallet
+  .signMessage(mockMessage)
+  .then((signature) => {
+    console.log("Signature:", signature)
     mockSignature = signature
-    verifyMessage({ message: mockMessage, address: mockAddress, signature: mockSignature })
+    verifyMessage({
+      message: mockMessage,
+      address: mockAddress,
+      signature: mockSignature,
+    })
   })
-  .catch(error => {
-    console.error("Error signing message:", error);
-  });
+  .catch((error) => {
+    console.error("Error signing message:", error)
+  })
 
-dotenv.config();
+dotenv.config()
 
 AppDataSource.initialize()
   .then(() => {
-    console.log("✅ Initialized data source");
+    console.log("✅ Initialized data source")
   })
   .catch((error) => {
-    console.error("❌ Could not initialize data source:", error);
-    process.exit(1);
-  });
+    console.error("❌ Could not initialize data source:", error)
+    process.exit(1)
+  })
 
-const app = express();
-app.use(express.json());
-
-
+const app = express()
+app.use(cors())
+app.use(express.json())
 
 // ============Routes=============
-app.use(userRoutes);
-app.use(postRoutes);
-app.use(fileRoutes);
-app.use(nonceRoutes);
+app.use(userRoutes)
+app.use(postRoutes)
+app.use(fileRoutes)
+app.use(nonceRoutes)
 
 app.listen(8000, () => {
-  console.log("Server running on port 8000");
-});
-
-
-
+  console.log("Server running on port 8000")
+})
